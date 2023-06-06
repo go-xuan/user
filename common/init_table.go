ea1483c8ec9f9e9ffb6a73d35a0f1a8748160fbe
quanchao <chao.quan@superdata-tech.com>
2023-06-07 01:58:18 +0800

    go-builder版本升级，调整相关代码

--- common/init_table.go
+++ common/init_table.go
@@ -7,55 +7,20 @@ import (
 
 // 初始化数据库表结构
 func InitGormTable() {
-	PGDB := gormx.CTL.DB
 	// 用户表
-	if PGDB.Migrator().HasTable(&entity.SysUser{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysUser{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysUser{})
-	}
+	_ = gormx.CTL.InitTable(&entity.SysUser{})
 	// 用户鉴权表
-	if PGDB.Migrator().HasTable(&entity.SysUserAuth{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysUserAuth{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysUserAuth{})
-	}
+	_ = gormx.CTL.InitTable(&entity.SysUserAuth{})
 	// 角色表
-	if PGDB.Migrator().HasTable(&entity.SysRole{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysRole{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysRole{})
-	}
-	// 用户角色表
-	if PGDB.Migrator().HasTable(&entity.SysRoleMember{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysRoleMember{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysRoleMember{})
-	}
-
+	_ = gormx.CTL.InitTable(&entity.SysRole{})
+	// 角色成员表
+	_ = gormx.CTL.InitTable(&entity.SysRoleMember{})
 	// 群组表
-	if PGDB.Migrator().HasTable(&entity.SysGroup{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysGroup{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysGroup{})
-	}
-	// 群组成员
-	if PGDB.Migrator().HasTable(&entity.SysGroupMember{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysGroupMember{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysGroupMember{})
-	}
-	// 群组所有角色
-	if PGDB.Migrator().HasTable(&entity.SysGroupRole{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysGroupRole{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysGroupRole{})
-	}
-
+	_ = gormx.CTL.InitTable(&entity.SysGroup{})
+	// 群组成员表
+	_ = gormx.CTL.InitTable(&entity.SysGroupMember{})
+	// 群组角色表
+	_ = gormx.CTL.InitTable(&entity.SysGroupRole{})
 	// 系统日志
-	if PGDB.Migrator().HasTable(&entity.SysLog{}) {
-		_ = PGDB.Migrator().AutoMigrate(&entity.SysLog{})
-	} else {
-		_ = PGDB.Migrator().CreateTable(&entity.SysLog{})
-	}
+	_ = gormx.CTL.InitTable(&entity.SysLog{})
 }
