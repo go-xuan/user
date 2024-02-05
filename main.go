1934564f642c3fa0251c7e669bf9c293392ba899
quanchao <virisred@gamil.com>
2024-02-05 09:53:24 +0800

    更新

--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"github.com/go-xuan/quanx"
+	"user/internal/model/entity"
 	"user/internal/router"
 )
 
 func main() {
 	var engine = quanx.GetEngine(quanx.EnableNacos)
 	engine.AddGinRouter(router.BindGinRouter)
-	//engine.SetConfigDir("conf")
-	//engine.AddTable(
-	//	&table.User{},
-	//	&table.UserAuth{},
-	//	&table.Role{},
-	//	&table.RoleUser{},
-	//	&table.Group{},
-	//	&table.GroupUser{},
-	//	&table.GroupRole{},
-	//	&table.log{},
-	//)
+	engine.SetConfigDir("conf")
+	engine.AddTable(
+		&entity.User{},
+		&entity.UserAuth{},
+		&entity.Role{},
+		&entity.RoleUser{},
+		&entity.Group{},
+		&entity.GroupUser{},
+		&entity.GroupRole{},
+		&entity.Log{},
+	)
 	// 初始化路由
 	engine.RUN()
 }
