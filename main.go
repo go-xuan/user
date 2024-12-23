932c7829172be064de6a510a378989563dab768a
quanchao <quanchao@jwzg.com>
2024-12-23 12:23:05 +0800

    开发进度保存

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"github.com/go-xuan/quanx"
+
 	"user/internal/model/entity"
 	"user/internal/router"
 )
 
 func main() {
 	var engine = quanx.NewEngine(
-		//app.EnableNacos,   // 启用nacos
-		//app.MultiDatabase, // 多数据源
-		quanx.MultiRedis,  // 对redis
-		quanx.MultiCache,  // 多缓存
-		quanx.EnableQueue, // 使用队列
-		quanx.Debug,       // debug模式
+		//quanx.EnableNacos,   // 启用nacos
+		quanx.MultiDatabase, // 多数据源
+		quanx.MultiRedis,    // 对redis
+		quanx.MultiCache,    // 多缓存
+		quanx.EnableQueue,   // 使用队列
+		quanx.Debug,         // debug模式
 	)
 
 	engine.AddGinRouter(router.BindGinRouter)
